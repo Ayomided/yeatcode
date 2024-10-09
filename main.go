2a6be538870447c785d92dafaef75da8005980b0
Ayomided <adediji.d@yahoo.com>
2024-10-09 22:17:27 +0100

    leetcode 122 Best time to buy and sell stock II

--- main.go
+++ main.go
@@ -323,3 +323,15 @@ func moveZeroes(nums []int) {
 		}
 	}
 }
+
+func maxProfitII(prices []int) int {
+	maxProfit := 0
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i+1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+
+	return maxProfit
+}
